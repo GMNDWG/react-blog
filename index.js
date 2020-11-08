7271853b01aca8a8c3acc620f3fdb5f1ecb1ec11
gmndwg <gemfru@yahoo.com>
2020-11-08 23:51:39 +0800

    move pivate information into dev.js

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
 const express = require('express');
-const app = express()
-const mongoose = require('mongoose')
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const config = require('./config/key')
+const { User } = require('./models/user')
 
-mongoose.connect('mongodb+srv://gmndwg:2381268@cluster0.25kfr.mongodb.net/<dbname>?retryWrites=true&w=majority',
-{useNewUrlParser:true, useUnifiedTopology:true}).then(() => console.log('DB connected'))
-                       .catch(err => console.log(err))
+
+mongoose.connect(config.mongoURI,
+{useNewUrlParser:true,
+useUnifiedTopology:true,
+useCreateIndex: true})
+                    .then(() => console.log('DB connected'))
+                    .catch(err => console.log(err))
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.get('/', (req,res) => {
     res.send("Hello 🌐")
 })
+
+app.post('/api/users/register',(req,res) =>{
+    const user = new User(req.body);
+    
+    user.save((err, doc) => {
+        if(err) return res.json ({ success: false, err})
+        res.status(200).json({
+        success: true,
+        userData: doc
+    })
+    })
+} )
+
 
 
 
