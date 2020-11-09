9398b184c20f3f23c99026eed1a37730b3d540e2
gmndwg <gemfru@yahoo.com>
2020-11-10 00:33:55 +0800

    heroku

--- server/index.js
+++ server/index.js
@@ -20,6 +20,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+
+app.get('/', (req,res) =>{
+    res.json({
+        "HEROKU": "DEPLOY ACCOUNT"
+    })
+})
+
 app.get('/api/user/auth',auth, (req,res) => {
     res.status(200).json({
         _id: req._id,
